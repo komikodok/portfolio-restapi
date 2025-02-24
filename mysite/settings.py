57b7e1b86559bc3341e966f3addc1c94dee14a62
komikodok <komikodok666@gmail.com>
2025-02-24 22:49:38 +0700

    minor update

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,8 +29,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
+    'corsheaders',
     'rest_framework',
+
     'api.apps.ApiConfig',
     'contact.apps.ContactConfig',
     'projects.apps.ProjectsConfig'
@@ -39,12 +41,18 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    ]
 
 ROOT_URLCONF = 'mysite.urls'
 
