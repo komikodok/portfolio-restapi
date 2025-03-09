babc48b9d11e33ffea42b438177e9c9946a763f1
komikodok <komikodok666@gmail.com>
2025-03-09 21:22:22 +0700

    add cors origin

--- mysite/settings.py
+++ mysite/settings.py
@@ -164,12 +164,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://komikodok.github.io',
     'https://fazport.up.railway.app'
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://komikodok.github.io',
     ]
 
 CORS_ALLOW_CREDENTIALS = True
