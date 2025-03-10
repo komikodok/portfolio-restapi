a23f00fdebf6624a5482667c4e2b29dbdc171232
komikodok <komikodok666@gmail.com>
2025-03-10 20:45:08 +0700

    minor update

--- assistant/serializers.py
+++ assistant/serializers.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
-from rest_framework.response import Response
-from django.conf import settings
 
 from .models import Assistant
-import os
+from django.conf import settings
 from cloudinary.utils import cloudinary_url
 
 
@@ -16,13 +14,8 @@ class AssistantSerializer(serializers.ModelSerializer):
 
     def get_image_url(self, obj):
         request = self.context.get("request")
-        allowed_referrers = [os.getenv('FRONTEND_URL')]
-        referer = request.META.get("HTTP_REFERER", "")
         
         if not settings.DEBUG:
-            if not any(domain in referer for domain in allowed_referrers):
-                return Response({"error": "Unauthorized"}, status=403)
-
             url, _ = cloudinary_url(
                 obj.image.public_id,
                 sign_url=True
--- projects/serializers.py
+++ projects/serializers.py
@@ -1,10 +1,8 @@
 from django.conf import settings
 from rest_framework import serializers
-from rest_framework.response import Response
 from cloudinary.utils import cloudinary_url
 
 from .models import Project
-import os
 
 
 class ProjectListSerializer(serializers.ModelSerializer):
@@ -27,13 +25,8 @@ class ProjectDetailSerializer(serializers.ModelSerializer):
 
     def get_image_url(self, obj):
         request = self.context.get("request")
-        allowed_referrers = [os.getenv('FRONTEND_URL')]
-        referer = request.META.get("HTTP_REFERER", "")
 
         if not settings.DEBUG:
-            if not any(domain in referer for domain in allowed_referrers):
-                return Response({"error": "Unauthorized"}, status=403)
-            
             url, _ = cloudinary_url(
                 obj.image.public_id,
                 sign_url=True
