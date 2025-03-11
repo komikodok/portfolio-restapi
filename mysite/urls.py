f19e70d693784a1f71a16b46ac84380bd6ebf7dc
komikodok <komikodok666@gmail.com>
2025-03-11 13:49:31 +0700

    add new env variable

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,14 @@
-from django.urls import path, re_path, include
+from django.urls import path, include
 from django.contrib import admin
 from django.conf import settings
 
+import os
 from .views import csrf_token
+from dotenv import find_dotenv, load_dotenv
 
+
+load_dotenv(find_dotenv())
 urlpatterns = [
-    path('kampretlegendbos/', admin.site.urls),
     path('csrf-token/', csrf_token),
     path('api/', include('api.urls')),
 ]
@@ -14,3 +17,6 @@ if settings.DEBUG:
     from django.conf.urls.static import static
 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if os.getenv('POSTGRES_LOCALLY') == 'True':
+    urlpatterns += [path('kampretlegendbos/', admin.site.urls)]
